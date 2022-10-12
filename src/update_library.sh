c260157bfda0c62dce393da7b253383204bf66b0
Jim Hendricks <dyreschlock@gmail.com>
2022-10-12 12:23:57 +0900

    library image conversion script now uses properties file // #2

--- src/update_library.sh
+++ src/update_library.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env zsh
 
+source _load_properties.sh
+
 declare -a platforms=("a26" "a52" "coleco" "gb" "gba" "gbc" "genesis" "gg" "intv" "msx" "nes" "ng" "ngp" "ngpc" "pce" "scd" "sdvmu" "sg1000" "sms" "snes" "vb" "vectrex" "wsc")
 
 for plat in "${platforms[@]}"; do
 
-  count=`ls -1 /volumes/pocket/tools/input/${plat}/*.bmp 2>/dev/null | wc -l`
-  if [ $count != 0 ]; then
+  image_directory=${preparation_library_directory}${plat}/
 
-    ./volumes/pocket/tools/AnaloguePocketLibraryImageConverter /volumes/pocket/tools/input/${plat}/*.bmp --output-dir=/volumes/pocket/system/library/images/${plat}
+  if [ -d $image_directory ]; then
 
+    count=`ls -1 ${image_directory}*.png 2>/dev/null | wc -l`
+    if [ $count != 0 ]; then
+
+      ${utility_directory}AnaloguePocketLibraryImageConverter ${image_directory}*.png --output-dir=${pocket_library_directory}${plat}
+
+    fi
   fi
 
 done
