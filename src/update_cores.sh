a6171a9066645ac8fa80a5cc98b903a32b183fca
Jim Hendricks <dyreschlock@gmail.com>
2022-11-02 12:17:47 +0900

    adding preserve platforms directory on updater // #1

--- src/update_cores.sh
+++ src/update_cores.sh
@@ -5,26 +5,7 @@ source _load_properties.sh
 pocket_platforms_directory=${pocket_directory}Platforms/
 
 #Run Updater Program
-
-${utility_directory}pocket_updater --all -p ${pocket_directory}
-
-#Copy overwrites
-overwrite_directory="${utility_directory}overwrite/"
-
-for f in ${overwrite_directory}Platforms/*.json; do
-
-  filename=${f#*Platforms/}
-  cp -v "$f" "${pocket_platforms_directory}${filename}"
-
-done
-
-for f in ${overwrite_directory}Platforms/_images/*.bin; do
-
-  filename=${f#*images/}
-  cp -v "$f" "${pocket_platforms_directory}_images/${filename}"
-
-done
-
+${utility_directory}pocket_updater --platformsfolder --all -p ${pocket_directory}
 
 echo "update complete"
 
