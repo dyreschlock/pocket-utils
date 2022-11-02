53b1cd96cab2bb63675213a95cdcd29c983fa2e0
Jim Hendricks <dyreschlock@gmail.com>
2022-11-03 08:18:11 +0900

    completed conversions and copies are put in the completed directory // #6

--- src/overwrite_platform_images.sh
+++ src/overwrite_platform_images.sh
@@ -5,7 +5,7 @@ source _load_properties.sh
 image_process_directory=${utility_directory}_image_processor/src/
 
 custom_platform_images_directory=${utility_directory}platform_images/
-custom_platform_images_overwrite_directory=${custom_platform_images_directory}_overwrites/
+custom_platform_images_completed_directory=${custom_platform_images_directory}_completed/
 
 # Create a BIN file for every PNG found in the custom platform images folder in the utility directory
 # BINs are put into the overwrite folder in the custom platform images folder
@@ -14,10 +14,12 @@ for file in ${custom_platform_images_directory}*.png; do
   filename=$(basename "$file" ".png")
 
   echo "Overwriting ${filename}"
-  output_file=${custom_platform_images_overwrite_directory}${filename}.bin
+  output_file=${custom_platform_images_directory}${filename}.bin
 
   #node create.js pce.png pce.bin
   node ${image_process_directory}create.js ${file} ${output_file}
+
+  mv "$file" "${custom_platform_images_completed_directory}${filename}.png"
 
 done
 
@@ -25,11 +27,13 @@ done
 
 pocket_platforms_images_directory=${pocket_directory}Platforms/_images/
 
-for file in ${custom_platform_images_overwrite_directory}*.bin; do
+for file in ${custom_platform_images_directory}*.bin; do
 
   filename=$(basename "$file")
 
   cp -v "$file" "${pocket_platforms_images_directory}${filename}"
+
+  mv "$file" "${custom_platform_images_completed_directory}${filename}"
 
 done
 
