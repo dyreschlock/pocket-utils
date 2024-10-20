c83262a0e3a8770dd987989ed9bdd5c0f898032b
Jim Hendricks <dyreschlock@gmail.com>
2024-10-20 19:56:44 +0900

    adding mgl information for more platforms // #27

--- src/com/schlock/pocket/entites/MisterMglInfo.java
+++ src/com/schlock/pocket/entites/MisterMglInfo.java
@@ -2,21 +2,56 @@ package com.schlock.pocket.entites;
 
 public enum MisterMglInfo
 {
+    //Sony
     PLAYSTATION("_Console/PSX", "1", "s", "1"),
 
+
+    //Nintendo
+    NINTENDO_64("_Console/N64", "1", "f", "1"),
     SUPER_NINTENDO("_Console/SNES", "2", "f", "0"),
+    FAMICOM_DISK("_Console/NES", "2", "f", "1"),
     NES("_Console/NES", "2", "f", "1"),
 
     GAMEBOY_ADVANCE("_Console/GBA", "2", "f", "1"),
     GAMEBOY_COLOR("_Console/Gameboy", "2", "f", "1"),
+    GAMEBOY("_Console/Gameboy", "2", "f", "1"),
 
+    POKEMON_MINI("_Console/PokemonMini", "1", "f", "1"),
+
+
+    //SNK
+    NEO_GEO_CD("_Console/NeoGeo", "1", "s", "1"),
+    NEO_GEO("_Console/NeoGeo", "1", "f", "1"),
+
+
+    //Sega
     SEGA_SATURN("_Console/Saturn", "1", "s", "0"),
+    SEGA_CD("_Console/MegaCD", "1", "s", "0"),
+    SEGA_GENESIS("_Console/MegaDrive", "1", "f", "1"),
+    SEGA_MASTER_SYSTEM("_Console/SMS", "1", "f", "1"),
+    SEGA_SG1000("_Console/ColecoVision", "1", "f", "2"),
 
     GAME_GEAR("_Console/SMS", "1", "f", "2"),
 
-    PC_ENGINE_CD("_Console/TurboGrafx16", "1", "s", "0"),
 
-    WONDERSWAN_COLOR("_Console/WonderSwan", "1", "f", "1");
+    //NEC
+    PC_ENGINE_CD("_Console/TurboGrafx16", "1", "s", "0"),
+    PC_ENGINE("_Console/TurboGrafx16", "1", "f", "0"),
+
+
+    //Atari
+    ATARI_7800("_Console/Atari7800", "1", "f", "1"),
+    ATARI_2600("_Console/Atari7800", "1", "f", "1"),
+
+    //Bandai
+    WONDERSWAN_COLOR("_Console/WonderSwan", "1", "f", "1"),
+
+
+    COLECOVISION("_Console/ColecoVision", "1", "f", "1"),
+
+    SUPERVISION("_Console/SuperVision", "1", "f", "1"),
+
+    ARCADE;
 
     String rbf;
     String delay;
@@ -29,6 +64,10 @@ public enum MisterMglInfo
         this.delay = delay;
         this.type = type;
         this.index = index;
+    }
+
+    MisterMglInfo()
+    {
     }
 
     public String getMglFilepath(PocketGame game)
